18aba8a5abe995f02927ec767fd1954e85fd4594
Aditya Desai <aditya.desai0199@gmail.com>
2020-12-07 11:06:51 +0530

    DFCL: 3

---
+++ Fourth Year/Trimester II/DFCL/Assignment 3/router_logs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import csv
+from prettytable import PrettyTable
+
+# Read the log file
+with open('logs.csv', 'r') as log:
+
+    # Parse as CSV
+    router_logs = list(csv.reader(log))[1:]
+
+    # Converting the parsed CSV into a proper table
+    table = PrettyTable(['Sr.', 'Source', 'Destination', 'Protocol', 'Count'])
+    for sr, row in enumerate(router_logs[1:]):
+        table.add_row([sr+1, *row, router_logs.count(row)])
+
+    # Printing the logs as a table
+    print(table)
